2324bb3274e51d95213c258a54265fd6e060a624
RhysComissiong <ttrmc@LNX-ELTS2-1.LUNET.LBORO.AC.UK>
2021-12-29 18:46:54 +0000

    chapter 2

---
+++ chapter-2/AdalineGD.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+
+class AdalineGD(object):
+    """Adaptive  Linear Neuron Classifier. 
+    
+    Parameters
+    -----------
+    eta : float
+        Learning rate (between 0.0 and 1.0)
+    n_iter : int
+        Passes over the training dataset.
+    random_state : int
+        Random number generator seed for random weight initialization
+        
+    Attributes
+    -----------
+    w_ : 1d-array
+        Weights after fitting
+    cost_ : list
+        Sum-of-squares cost function value in each epoch.
+        
+    """
+    def __init__(self,eta=0.01,n_iter=50,random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+        
+    def fit(self, X, y):
+        """ Fit training data.
+        
+        Parameters
+        -----------
+        X: {array-like}, shape = [n_examples, n_features]
+            Training vectors, where n_examples is the number of examples and n_features is the number of features.
+        y: array-like, shape = [n_examples]
+            Target values.
+            
+        Returns
+        ---------
+        self: object
+        
+        """
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        self.cost_ = []
+        
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors) # feature matrix and error vector
+            self.w_[0] += self.eta * errors.sum() # gradient based on whole training dataset
+            cost = (errors**2).sum() / 2.0
+            self.cost_.append(cost)
+        return self
+    
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+    
+    def activation(self, X):
+        """Compute linear activation"""
+        return X # no effect in this code = Identity function
+    
+    def predict(self, X):
+        return np.where(self.activation(self.net_input(X)) >= 0.0, 1, -1)
+        
+
